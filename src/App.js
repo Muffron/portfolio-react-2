4c36204da3e97bd6934ee376e0ab24fc4204b78e
19523197 <19523197@students.uii.ac.id>
2023-05-05 19:43:26 -0800

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home";
 import Navbar from "./components/navbar";
 import Skills from "./components/skills";
 import Work from "./components/work";
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Work />
           <Contact />
         </section>
+        <Analytics />
       </main>
     </div>
   );
